851d56d8553ec491f31ea07dc0514d74214023b9
Sohaib Sohail <sohaibsohail@Sohaibs-MacBook-Air.local>
2025-07-26 00:24:36 +0100

    Ecr

--- ecr.sh
+++ ecr.sh
@@ -7,7 +7,7 @@ REPO_NAME="sohaibsohail/sohaib-portfolio"
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 # Build Docker image
-docker build -t $REPO_NAME .
+docker build -t $REPO_NAME . --platform linux/amd64
 
 # Tag image for ECR
 docker tag $REPO_NAME:latest $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:latest

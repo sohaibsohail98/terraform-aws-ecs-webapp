895e06f3bcc8d8b45d002d0fd030871142ac70c1
Sohaib Sohail <sohaibsohail@Sohaibs-MacBook-Air.local>
2025-07-27 04:10:19 +0100

    ecr policies readded

--- infra/modules/ecs/ecr.tf
+++ infra/modules/ecs/ecr.tf
@@ -3,6 +3,50 @@ data "aws_ecr_repository" "app_repo" {
   name = var.ecr_repository_name
 }
 
+# ECR Repository Policy
+resource "aws_ecr_repository_policy" "app" {
+  repository = data.aws_ecr_repository.app_repo.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowPull"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+      }
+    ]
+  })
+}
+
+# ECR Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = data.aws_ecr_repository.app_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 data "aws_ecr_image" "app_image" {
   repository_name = data.aws_ecr_repository.app_repo.name
   image_tag       = var.image_tag # e.g. "latest" or your timestamp tag
